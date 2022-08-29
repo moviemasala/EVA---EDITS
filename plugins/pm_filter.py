a4cecc7d4bfc1439311aaf8c2c25528b7c7a79ea
DARK-WEBLOAD-TG <112357595+DARK-WEBLOAD-TG@users.noreply.github.com>
2022-08-29 18:49:24 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -838,9 +838,9 @@ async def advantage_spell_chok(msg):
         )
     ] for k, movie in enumerate(movielist)]
     btn.append([InlineKeyboardButton(text="Close ⛔", callback_data=f'spolling#{user}#close_spellcheck')])
-    await msg.reply("<b>I couldn't find anything related to that Did you mean any one of these?\n\nനിങ്ങൾ ഉദ്ദേശിച്ച സിനിമ താഴെ കാണുന്നത് വല്ലതും ആണെങ്കിൽ അതിൽ ക്ലിക്ക് ചെയ്യുക</b>",
-                    reply_markup=InlineKeyboardMarkup(btn))
-
+    m = await msg.reply("<b>I couldn't find anything related to that Did you mean any one of these?\n\nനിങ്ങൾ ഉദ്ദേശിച്ച സിനിമ താഴെ കാണുന്നത് വല്ലതും ആണെങ്കിൽ അതിൽ ക്ലിക്ക് ചെയ്യുക</b>", reply_markup=InlineKeyboardMarkup(btn))
+    await asyncio.sleep(20)
+    await m.delete()
 
 async def manual_filters(client, message, text=False):
     group_id = message.chat.id
